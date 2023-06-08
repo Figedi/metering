128b6ad1cbcc05054d3937197da90eb9325c4d3b
Felix Kaminski <kaminski.felix@gmail.com>
2023-06-08 13:22:45 -0700

    fix: updates outdated deps

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export * from "./lib/timers";
 export * from "./lib/errors";
 export * from "./lib/metricTypes";
 export * from "./lib/MonitoringServer";
-
 // =========== experimental fastify metrics for routes via SkeLLLa/fastify-metrics ===========
 export * from "./lib/ServerRecorder";
 export * from "./lib/serverTypes";
